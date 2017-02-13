363a78b955d581d662b5132f4dce51b611530a88
Marco <mvinicio@uees.edu.ec>
2017-02-13 13:05:54 -0500

    configuracion inicial

---
+++ ChatBotAPI/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAcJX77ambkBAPEOynhNdbwLZBndle2Ddg28ImSi0HHX5DDQ0oU6KafgUYOa8xGW9W13gZCOIRaUpZC4jtZCmQr1z2VpZByubrJ9ZB1kIhHzVISFpT24r6QpHgE61Q5ZAW7ZA54QtU2kgGcF5cHiErM4n0OR3SeEnEmsUakIfGZAiWwZDZD',
+    FB_VERIFY_TOKEN: 'mitoken',
+    API_AI_CLIENT_ACCESS_TOKEN: 'ac17f98be19f4d5fbd2c4a968bf7280b',
+    FB_APP_SECRET: '44871f34d67ab638bc9ca3b6a508ab04',
+    SERVER_URL: " https://chatbotueesmediaplus.herokuapp.com/",
+};
